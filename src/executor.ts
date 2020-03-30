60e3038cd5389661ef724afe1ee5185ce1d3125a
Anton <choixer@gmail.com>
2020-03-30 21:06:07 +0200

    Add strings and print

--- src/executor.ts
+++ src/executor.ts
@@ -6,10 +6,15 @@ const sum = (atom: Atom): number => {
   return values.reduce(sumFn, 0);
 };
 
-const mult = (atom: Atom): number => {
+const multiply = (atom: Atom): number => {
   const values = atom.adjacentAtoms.map(childAtom => executor(childAtom));
-  const multFn = (total: number, val: number) => total *= val;
-  return values.reduce(multFn, 1);
+  const multiplyFn = (total: number, val: number) => total *= val;
+  return values.reduce(multiplyFn, 1);
+};
+
+const println = (atom: Atom): string => {
+  const values = atom.adjacentAtoms.map(childAtom => executor(childAtom));
+  return values.join(" ");
 };
 
 export default function executor(atom: Atom): any {
@@ -21,11 +26,22 @@ export default function executor(atom: Atom): any {
       break;
 
     case "*":
-      result = mult(atom);
+      result = multiply(atom);
+      break;
+
+    case "print":
+      result = println(atom);
       break;
 
     case (atom.value.match(/^[0-9]*$/) || {}).input:
       result = parseInt(atom.value);
+      break;
+
+    case (atom.value.match(/^"(?:\\.|[^\\"])*"$/) || {}).input:
+      return atom.value.slice(1, atom.value.length-1).replace(/\\(.)/g, (_, c) => {
+        return c === "n" ? "\n" : c;
+      });
+
     default:
       break;
   }
