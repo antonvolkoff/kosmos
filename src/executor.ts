6ae7364065ec1ce513e69fcde5c2c2fc7cad7b44
Anton <choixer@gmail.com>
2020-03-30 21:33:35 +0200

    Restructure executor code

--- src/executor.ts
+++ src/executor.ts
@@ -1,50 +1,45 @@
 import Atom from "./atom";
 
-const sum = (atom: Atom): number => {
-  const values = atom.adjacentAtoms.map(childAtom => executor(childAtom));
-  const sumFn = (sum: number, val: number) => sum += val;
-  return values.reduce(sumFn, 0);
-};
-
-const multiply = (atom: Atom): number => {
-  const values = atom.adjacentAtoms.map(childAtom => executor(childAtom));
-  const multiplyFn = (total: number, val: number) => total *= val;
-  return values.reduce(multiplyFn, 1);
-};
-
-const println = (atom: Atom): string => {
-  const values = atom.adjacentAtoms.map(childAtom => executor(childAtom));
-  return values.join(" ");
-};
+let env = new Map();
 
 export default function executor(atom: Atom): any {
   let result: any = "ERROR";
 
   switch (atom.value) {
-    case "+":
-      result = sum(atom);
-      break;
-
-    case "*":
-      result = multiply(atom);
-      break;
-
-    case "print":
-      result = println(atom);
-      break;
-
     case (atom.value.match(/^[0-9]*$/) || {}).input:
       result = parseInt(atom.value);
       break;
 
     case (atom.value.match(/^"(?:\\.|[^\\"])*"$/) || {}).input:
-      return atom.value.slice(1, atom.value.length-1).replace(/\\(.)/g, (_, c) => {
+      result =  atom.value.slice(1, atom.value.length-1).replace(/\\(.)/g, (_, c) => {
         return c === "n" ? "\n" : c;
       });
 
     default:
+      if (env.has(atom.value)) {
+        const fn = env.get(atom.value);
+        return fn(atom);
+      }
+
       break;
   }
 
   return result;
 };
+
+env.set("+", (atom: Atom) => {
+  const values = atom.adjacentAtoms.map(childAtom => executor(childAtom));
+  const sumFn = (sum: number, val: number) => sum += val;
+  return values.reduce(sumFn, 0);
+});
+
+env.set("*", (atom: Atom) => {
+  const values = atom.adjacentAtoms.map(childAtom => executor(childAtom));
+  const multiplyFn = (total: number, val: number) => total *= val;
+  return values.reduce(multiplyFn, 1);
+});
+
+env.set("print", (atom: Atom) => {
+  const values = atom.adjacentAtoms.map(childAtom => executor(childAtom));
+  return values.join(" ");
+});
