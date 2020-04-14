0c38c983ed26ce9b6bcbdaa007fd60d84d05bea0
Anton <choixer@gmail.com>
2020-04-14 23:20:53 +0200

    Full selected highlight

--- src/shapes/atom_v2_shape.ts
+++ src/shapes/atom_v2_shape.ts
@@ -12,6 +12,10 @@ export default {
     s.fill(s.color('#ffffff'));
     s.stroke(s.color('#999999'));
     s.strokeWeight(1.25);
+    if (atom.selected) {
+      s.stroke(s.color('#1DA159'));
+      s.strokeWeight(2);
+    }
 
     const x = atom.x - 20;
     const y = atom.y - 20;
@@ -24,11 +28,6 @@ export default {
     }
 
     s.rect(x, y, width + widthExtension, height, BORDER_RADIUS);
-
-    if (atom.selected) {
-      s.stroke(s.color('#1DA159'));
-      s.strokeWeight(2);
-    }
     s.circle(atom.x, atom.y, 16);
 
     s.push();
