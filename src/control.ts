c28229160c44828b224d39c7b64ce18cab1c7090
Anton <choixer@gmail.com>
2020-04-17 11:28:35 +0200

    Move evaluate icon into separete file

--- src/control.ts
+++ src/control.ts
@@ -1,17 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { html } from "htm/react";
+
 import { State } from "./state";
+import PlayIcon from "./play_icon";
 
 interface Props {
   state: State;
-}
-
-function PlayIcon() {
-  return html`
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-play">
-      <polygon points="5 3 19 12 5 21 5 3"></polygon>
-    </svg>
-  `;
 }
 
 export default function Control({ state }: Props) {
