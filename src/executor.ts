381225a4e9fdb8eec7eddf9e29aa867b53283a92
Anton <choixer@gmail.com>
2020-04-27 18:07:17 +0200

    Start nRepl and connect to it
    It uses nrepl port file

--- src/executor.ts
+++ src/executor.ts
@@ -1,14 +1,52 @@
-import * as util from 'util';
+import { promisify } from 'util';
 import * as repl from "nrepl-client";
+import { homedir } from "os";
+import { readFileSync } from "fs";
 
-const replClient = repl.connect({ port: 58597 });
-replClient.once('connect', () => {
-  console.log("Connected to REPL");
-});
+type NReplState = "waiting-for-port" | "connecting" | "connected";
 
-const evalPromise = util.promisify(replClient.eval);
+type NRepl = {
+  state: NReplState;
+  port: number | undefined;
+  client: any;
+};
+
+const nReplPortFile = `${homedir()}/.nrepl-port`;
+
+const process = (nRepl: NRepl): NRepl => {
+  switch (nRepl.state) {
+    case "waiting-for-port":
+      const buffer = readFileSync(nReplPortFile);
+      nRepl.port = parseInt(buffer.toString());
+      nRepl.state = "connecting";
+      setTimeout(processRepl, 1000);
+      break;
+
+    case "connecting":
+      nRepl.client = repl.connect({ port: nRepl.port });
+      nRepl.state = "connected";
+      setTimeout(processRepl, 1000);
+      break;
+
+    case "connected":
+      setTimeout(processRepl, 10000);
+      break;
+  }
+  return nRepl;
+}
+
+let nRepl: NRepl = {
+  state: "waiting-for-port",
+  port: undefined,
+  client: undefined,
+};
+
+const processRepl = () => process(nRepl);
+
+setTimeout(processRepl, 1000);
 
 export default async function executor(code: string): Promise<string> {
+  const evalPromise = promisify(nRepl.client.eval);
   const result = await evalPromise(code);
   return result[0].out || result[0].value;
 };
