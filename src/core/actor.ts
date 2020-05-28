e048f0bdd2d88ea75a1b07e4d53b2ef0ccb04b45
Anton <choixer@gmail.com>
2020-05-28 23:56:41 +0200

    Simplify actor API
    - remove inderect code and event emitter as middleman

--- src/core/actor.ts
+++ src/core/actor.ts
@@ -1,18 +1,8 @@
-import { EventEmitter } from "events";
-
 type Address = string;
 
-interface Message {
-  type: string;
-  payload?: any;
-  resolve?: Function;
-};
-
-export interface Actor {
-  init?: (message: Message) => any;
-  handleCall?: (state: any, message: Message, resolve: Function) => any;
-  handleSend?: (state: any, message: Message) => any;
-  terminate?: (state: any, message: Message) => any;
+export interface Behavior {
+  init?: () => any;
+  terminate?: (state: any) => any;
   [key: string]: any;
 }
 
@@ -27,63 +17,38 @@ function createIdGenerator(prefix = "actor/", start = 0) {
 };
 
 const id = createIdGenerator();
-const mailbox = new EventEmitter();
+const behaviors = {};
 const states = {};
-const defaultActor: Actor = {
-  init(message) {
-    return null;
-  },
-  handleSend(state, message) {
-    return state;
-  },
-  handleCall(state, message, resolve) {
-    resolve(state);
-    return state;
-  },
-  terminate(state, message) {
-  },
+const defaultBehavior: Behavior = {
+  init: () => null,
+  terminate: (state) => state,
 };
 
-export function start(behavior: Actor, name: string = ""): Address {
-  const actor = { ...defaultActor, ...behavior };
-  const address = id.next();
-  states[address] = actor.init({ type: "init" });
+export function start(behavior: Behavior, name: string = ""): Address {
+  const address = name == "" ? id.next() : name;
+  const actor = { ...defaultBehavior, ...behavior };
 
-  const handleMailboxMessage = (message: Message) => {
-    const { type, resolve } = message;
-    if (resolve) {
-      states[address] = actor.handleCall(states[address], message, resolve);
-    } else if (type == "terminate") {
-      states[address] = actor.terminate(states[address], message);
-    } else {
-      states[address] = actor.handleSend(states[address], message);
-    }
-  };
-
-  mailbox.on(address, handleMailboxMessage);
-  if (name != "") {
-    mailbox.on(name, handleMailboxMessage);
-  }
+  behaviors[address] = actor;
+  states[address] = send(address, "init");
 
   return address;
 };
 
-export function send(address: Address, message: Message) {
-  mailbox.emit(address, message);
+export function send(address: Address, message: string, payload: any = null) {
+  const fn = behaviors[address][message];
+  states[address] = fn(states[address], payload);
 };
 
-export async function call(address: Address, message: Message): Promise<any> {
-  return new Promise(resolve => {
-    mailbox.emit(address, { ...message, resolve });
-  });
-};
+export function call(address: string, message: string, payload: any = undefined): any {
+  const fn = behaviors[address][message];
+  const { state, response } = fn(states[address], payload);
+  states[address] = state;
+  return response;
+}
 
 export function stop(address: Address) {
-  send(address, { type: "terminate" });
-  mailbox.removeAllListeners(address);
+  send(address, "terminate");
+  delete behaviors[address];
   delete states[address];
 };
 
-export function info(address: Address): any {
-  return states[address];
-}
