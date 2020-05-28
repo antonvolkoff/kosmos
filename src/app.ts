7d06efcc79d129f189295e4d298e7cf5f864e0f2
Anton <choixer@gmail.com>
2020-05-28 23:57:15 +0200

    Wrap global store as an Actor

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,19 @@ import Window from "./window";
 import Interface from "./interface";
 import Canvas from "./canvas";
 import Repl from "./repl";
+import "./workspace";
+import { start } from "./core/actor";
 
 const store = createApplicationStore();
+
+const StoreBehavior = {
+  dispatch: (state, action) => {
+    store.dispatch(action);
+    return state;
+  },
+  getState: (state) => ({ state, response: store.getState() }),
+};
+start(StoreBehavior, "store");
 
 Window(store);
 AppMenu(store);
