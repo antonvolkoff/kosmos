2321f93c7474ed10d10b861473a4dbe9f860600a
Anton Volkov <choixer@gmail.com>
2020-11-18 14:38:26 +0100

    Draw grid points
    Place paperjs canvas under p5 canvas and render background dot grid

--- src/kosmos/canvas/views.cljs
+++ src/kosmos/canvas/views.cljs
@@ -1,22 +1,31 @@
 (ns kosmos.canvas.views
   (:require [reagent.core :as r]
             [reagent.dom :as rdom]
-            [paper :as paper]))
+            [paper :as paper]
+            [kosmos.canvas.grid :refer [make-grid]]))
+
+(def background-color "#fdfdfd")
+
+(def grid-color "#ccc")
 
 (defn- canvas [{:keys [on-mount]}]
   (r/create-class
    {:component-did-mount #(on-mount (rdom/dom-node %))
-    :reagent-render #(-> [:canvas])}))
+    :reagent-render #(-> [:canvas {:resize ""}])}))
 
-(defn- paper-canvas [{:keys [on-mount]}]
-  [canvas {:on-mount #(-> % paper/setup on-mount)}])
-
-(defn render []
-  ;; Examples of drawing path and text
-  (new paper/Path.Rectangle
-       #js {:from #js [0 0] :to #js [150 150] :strokeColor "black"})
-  (new paper/PointText
-       #js {:point #js [50 50] :content "Hey" :fontSize 16}))
+(defn- render [^paper/PaperScope scope]
+  (let [view ^paper/CanvasView (.-view scope)
+        width (-> view .-bounds .-width)
+        height (-> view .-bounds .-height)
+        bg-rect #js {:from #js [0 0] :to #js [width height] :fillColor background-color}
+        dot-circles (map (fn [center]
+                           #js {:center (clj->js center) :radius 1.25 :fillColor grid-color})
+                         (make-grid width height))]
+    (new paper/Path.Rectangle bg-rect)
+    (dorun (map #(new paper/Path.Circle %) dot-circles))))
 
 (defn background []
-  [paper-canvas {:on-mount render}])
+  [canvas {:on-mount #(-> % paper/setup render)}])
+
+(comment
+  (paper/install js/window))
