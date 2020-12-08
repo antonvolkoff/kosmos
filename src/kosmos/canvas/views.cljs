b407bfa4de8be8e3e58ae6907fe18f42e90d4f59
Anton Volkov <choixer@gmail.com>
2020-12-08 10:03:31 +0100

    Use paperjs wrapper for react to render background

--- src/kosmos/canvas/views.cljs
+++ src/kosmos/canvas/views.cljs
@@ -1,29 +1,19 @@
 (ns kosmos.canvas.views
-  (:require [reagent.core :as r]
-            [reagent.dom :as rdom]
-            [paper :as paper]
+  (:require [kosmos.paper :refer [paper-container rectangle circle layer]]
             [kosmos.canvas.grid :refer [make-grid]]))
 
 (def background-color "#fdfdfd")
 
 (def grid-color "#ccc")
 
-(defn- canvas [{:keys [on-mount]}]
-  (r/create-class
-   {:component-did-mount #(on-mount (rdom/dom-node %))
-    :reagent-render #(-> [:canvas {:resize ""}])}))
+(def canvas-size [3840 2160]) ; This is an 4K size
 
-(defn- render [^paper/PaperScope _scope]
-  (let [size [3840 2160] ; This is an 4K size
-        bg-rect #js {:from #js [0 0] :to (clj->js size) :fillColor background-color}
-        dot-circles (map (fn [center]
-                           #js {:center (clj->js center) :radius 1.25 :fillColor grid-color})
-                         (make-grid size))]
-    (new paper/Path.Rectangle bg-rect)
-    (dorun (map #(new paper/Path.Circle %) dot-circles))))
+(def dots (map (fn [center] {:center center :radius 1.25 :fill-color grid-color})
+               (make-grid canvas-size)))
 
 (defn background []
-  [canvas {:on-mount #(-> % paper/setup render)}])
-
-(comment
-  (paper/install js/window))
+  [paper-container
+   [layer
+    [rectangle {:from [0 0] :size canvas-size :fill-color background-color}]
+    (map-indexed (fn [idx dot] ^{:key (str "dot-" idx)} [circle dot])
+                 dots)]])
---
+++ test/kosmos/paper_test.cljs
@@ -0,0 +1,2 @@
+(ns kosmos.paper-test)
+
