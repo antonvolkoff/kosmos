b35cb7a29028400ea6d96575733e0589ef77510d
Anton Volkov <choixer@gmail.com>
2020-12-10 12:42:42 +0100

    Switch from paperjs to svg

--- src/kosmos/canvas/views.cljs
+++ src/kosmos/canvas/views.cljs
@@ -1,6 +1,5 @@
 (ns kosmos.canvas.views
   (:require [re-frame.core :refer [subscribe]]
-            [kosmos.paper :refer [paper-container rectangle circle layer line]]
             [kosmos.canvas.grid :refer [make-grid]]
             [kosmos.canvas.subs]
             [clojure.data]))
@@ -13,7 +12,7 @@
 
 (def canvas-size [3840 2160]) ; This is an 4K size
 
-(def dots (map (fn [center] {:center center :radius 1.25 :fill-color grid-color})
+(def dots (map (fn [center] {:center center :radius 1.25 :fill grid-color})
                (make-grid canvas-size)))
 
 (defn edge-point
@@ -22,16 +21,21 @@
 
 (defn edge [{:keys [source-id target-id]}]
   (let [source @(subscribe [:canvas/node source-id])
-        target @(subscribe [:canvas/node target-id])]
-    [line {:from (edge-point source) :to (edge-point target) :stroke-color edge-color}]))
+        target @(subscribe [:canvas/node target-id])
+        [x1 y1] (edge-point source)
+        [x2 y2] (edge-point target)]
+    [:line {:x1 x1 :y1 y1 :x2 x2 :y2 y2 :stroke edge-color}]))
+
+(defn dot [{:keys [center radius fill]}]
+  (let [[cx cy] center]
+    [:circle {:cx cx :cy cy :r radius :fill fill}]))
 
 (defn background []
-  (let [edges @(subscribe [:canvas/edges])]
-    [paper-container
-     [layer
-      [rectangle {:from [0 0] :size canvas-size :fill-color background-color}]
-      (map-indexed (fn [idx dot] ^{:key (str "dot-" idx)} [circle dot])
-                   dots)]
-     [layer
-      (map (fn [edge-attrs] [edge edge-attrs])
-           edges)]]))
+  (let [edges @(subscribe [:canvas/edges])
+        [width height] canvas-size]
+    [:svg {:width 800 :height 800}
+     [:g
+      [:rect {:x 0 :y 0 :width width :height height :fill background-color}]
+      (map-indexed (fn [idx attrs] ^{:key (str "dot-" idx)} [dot attrs]) dots)]
+     [:g
+      (map (fn [edge-attrs] [edge edge-attrs]) edges)]]))
