873fe7c8703dcb79de5ec8cab33a379fea1d4aff
Anton Volkov <choixer@gmail.com>
2020-12-22 15:42:22 +0100

    IO operation on text files
    Editor can open, modify and save text files via REPL promt.

--- src/kosmos/main.clj
+++ src/kosmos/main.clj
@@ -1,18 +1,32 @@
 (ns kosmos.main
-  (:require [nrepl.server :as nrepl]
+  (:require [clojure.string :as str]
+            [nrepl.server :as nrepl]
             [kosmos.window :as window])
   (:import [org.jetbrains.skija Paint Font FontMgr FontStyle]))
+
+(defonce lines (atom ""))
+
+(defn read-lines [path]
+  (str/split-lines (slurp path)))
+
+(defn remove-at [coll n]
+  (concat (take n coll) (drop (inc n) coll)))
+
+(defn save-lines [path lines]
+  (spit path (str/join "\r\n" lines)))
 
 (defn color [rgba]
   (.intValue (Long/valueOf rgba)))
 
 (defn draw [canvas]
   (.clear canvas (color 0xFFF0F0F0))
-  (let [typeface (-> (FontMgr/getDefault) (.matchFamilyStyle "monospace" FontStyle/NORMAL))
+  (let [typeface (-> (FontMgr/getDefault) (.matchFamilyStyle "JetBrains Mono" FontStyle/NORMAL))
         font (-> (new Font) (.setTypeface typeface) (.setSize 32))
         black-paint (-> (new Paint) (.setColor (color 0xFF000000)))]
-    (.drawString canvas "(defn hello []" 40 60 font black-paint)
-    (.drawString canvas "(print \"Hello\"))" 60 110 font black-paint)))
+    (doall
+     (map-indexed (fn [idx line]
+                    (.drawString canvas line 40 (+ 60 (* idx 44)) font black-paint))
+                  @lines))))
 
 (defn keypress [_win _key _scancode _action _mods]
   true)
@@ -25,3 +39,13 @@
                   :title "Kosmos"
                   :on-draw draw
                   :on-keypress keypress}))
+
+(comment
+  ; Open file
+  (reset! kosmos.main/lines (kosmos.main/read-lines "/Users/antonvolkoff/Code/github.com/antonvolkoff/kosmos/src/kosmos/fx.cljs"))
+
+  ; Edit
+  (swap! kosmos.main/lines conj "; Adding comment")
+
+  ; Save file
+  (kosmos.main/save-lines "/Users/antonvolkoff/Code/github.com/antonvolkoff/kosmos/src/kosmos/fx.cljs" @kosmos.main/lines))
